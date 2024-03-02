ee8f650b6b45716d888d340f013b112104bc07fc
envivo2024 <160994526+envivo2024@users.noreply.github.com>
2024-03-02 17:23:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Simulación de datos de eventos
+const events = [
+    { time: "19:00", teams: "UCV vs CuscoFC, Debut Paolo Guerrero" },
+    { time: " ", teams: " " },
+    // Añade más eventos según sea necesario
+];
+
+function displayEvents() {
+    const eventListElement = document.getElementById('event-list');
+    eventListElement.innerHTML = '';
+
+    events.forEach(event => {
+        eventListElement.innerHTML += `<div>${event.time} - ${event.teams}</div>`;
+    });
+
+    document.getElementById('last-updated-time').textContent = new Date().toLocaleTimeString();
+}
+let devToolsOpen = false;
+
+
+
+
+// Actualizar los eventos cada minuto
+setInterval(displayEvents, 60000);
+
+// Mostrar los eventos al cargar la página
+displayEvents();
 
